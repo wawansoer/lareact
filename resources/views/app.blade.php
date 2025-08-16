1891bf3bb1328aaa329210bcebf518a0f326a04b
Sutisna Utamasjida Septiawan <sutisnautmasjida@gmail.com>
2025-08-17 05:32:37 +0700

    style(ui): update color variables and font families
    - Update color variables in app.css for both light and dark themes
    - Change font families to IBM Plex Sans, Serif, and Mono
    - Remove unused font preconnect links in welcome.tsx
    - Update user name styling in user-info.tsx component

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,7 +38,7 @@
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono:ital,wght@1,500&family=DM+Sans:ital,opsz,wght@1,9..40,500&family=IBM+Plex+Mono:ital,wght@0,500;0,600;1,500;1,600&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@500&family=IBM+Plex+Sans:wght@500&family=IBM+Plex+Serif:wght@400;500&display=swap" rel="stylesheet">
 
         @routes
         @viteReactRefresh
